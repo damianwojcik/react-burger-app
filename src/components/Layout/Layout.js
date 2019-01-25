6d52c8be4ae8f52c219e4baf104c0115feb44633
Damian Wójcik <damian.wojcik@agora.pl>
2019-01-25 14:19:45 +0100

    basic app structure

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import styles from './Layout.module.css';
+
+const Layout = ( props ) => (
+    <>
+        <div>
+            toolbar sidedraver backdrop
+        </div>
+        <main className={styles.Content}>
+            {props.children}
+        </main>
+    </>
+);
+
+export default Layout;
