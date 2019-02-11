4f95998b783eb65af2cdd7b8234d31d05d1ce839
Damian Wójcik <damian.wojcik@agora.pl>
2019-02-11 08:23:14 +0100

    added CheckoutSummary container

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import Checkout from './containers/Checkout/Checkout'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>test</p>
+          <BurgerBuilder />
+          <Checkout />
         </Layout>
-        <BurgerBuilder />
       </div>
     );
   }
